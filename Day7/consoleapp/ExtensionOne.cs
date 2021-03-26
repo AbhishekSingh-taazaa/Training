6746dd46c078160974bd8ede4a4550013fc1c1e5
AbhishekSingh-taazaa <abhishek.singh@taazaa.com>
2021-03-26 18:14:36 +0530

    day7 commit

---
+++ Day7/consoleapp/ExtensionOne.cs
@@ -0,0 +1,17 @@
+﻿using core;
+using infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consoleapp
+{
+   public static class ExtensionOne
+    {
+         
+        public static void Display(this Inventory I ) {
+
+            Console.WriteLine(I.ToString());
+        }
+    }
+}
---
+++ Day7/core/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace core
+{
+    public class Product
+    {
+        public string title { get; set; }
+
+        public decimal price { get; set; }
+
+        public string color { get; set; }
+    }
+}
