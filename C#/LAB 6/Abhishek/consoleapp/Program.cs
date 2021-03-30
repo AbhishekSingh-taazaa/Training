f7ebe754250edb1caef13b3322042464deda0c1c
AbhishekSingh-taazaa <abhishek.singh@taazaa.com>
2021-03-30 23:57:33 +0530

    LAB 6

---
+++ C#/LAB 6/Abhishek/consoleapp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using infrastructure;
+
+
+namespace consoleapp
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+           infrastructure.Inventory i1 = new Inventory();
+
+            i1.AddProduct("Rex",23,"Olive");
+            var y = i1.ToString(); 
+
+            Console.WriteLine(y);
+            i1.Display();
+        }
+    }
+}
