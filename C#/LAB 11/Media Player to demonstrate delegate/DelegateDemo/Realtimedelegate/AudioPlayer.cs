50e4f8788197f101f0a5ae3691498c95d67ebb59
AbhishekSingh-taazaa <abhishek.singh@taazaa.com>
2021-03-31 00:00:54 +0530

    LAB 11

---
+++ C#/LAB 11/Media Player to demonstrate delegate/DelegateDemo/Realtimedelegate/AudioPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Realtimedelegate
+{
+   public  class AudioPlayer
+    {
+        private int audiostatus;
+
+        public int AudioFile(string extension ) {
+
+            if (extension != "wmv")
+            {
+
+                audiostatus = -1;
+               
+
+            }
+
+            else {
+
+                audiostatus = 0;
+            
+            }
+
+
+            return audiostatus;
+
+        }
+
+    }
+}
---
+++ C#/LAB 11/Media Player to demonstrate delegate/DelegateDemo/Realtimedelegate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Realtimedelegate
+{
+   public class Program
+    {
+       public static void Main(string[] args)
+        {
+            MediaPlayer Mediaplayer = new MediaPlayer();
+            VideoPlayer Videoplayer = new VideoPlayer();
+            AudioPlayer AudioPlayer = new AudioPlayer();
+
+            MediaPlayer.PlayerStatus statusdele = Videoplayer.VideoFile;
+
+            
+            Mediaplayer.MediaResult(statusdele, "mkv");
+
+            statusdele = AudioPlayer.AudioFile;
+
+            Mediaplayer.MediaResult(statusdele,"mvc");
+           
+        }
+    }
+}
