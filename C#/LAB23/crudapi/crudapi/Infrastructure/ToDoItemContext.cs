7e447be4a5f72482dddb5a26394de1c98ca84381
AbhishekSingh-taazaa <abhishek.singh@taazaa.com>
2021-04-13 09:16:15 +0530

    LAB23

---
+++ C#/LAB23/crudapi/crudapi/Infrastructure/ToDoItemContext.cs
@@ -0,0 +1,18 @@
+﻿using crudapi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crudapi.Infrastructure
+{
+    public class ToDoItemContext : DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+        public ToDoItemContext(DbContextOptions<ToDoItemContext> options) : base(options)
+        { 
+        }
+    }
+}
---
+++ C#/LAB23/crudapi/crudapi/Startup.cs
@@ -0,0 +1,62 @@
+using crudapi.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crudapi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ToDoItemContext>(opt => opt.UseSqlServer(Configuration["ConnectionStrings:ToDoConnectionstring"]));
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "crudapi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "crudapi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
